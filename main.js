e46af2119fa3b54812d63d4e1ca050c66d5fff7a
Rita <rita.anierobi@stu.ocsb.ca>
2024-11-02 21:19:48 +0000

    6

--- main.js
+++ main.js
@@ -73,27 +73,163 @@ document.getElementById("start_game").addEventListener("click", startGame);
 function startGame() {
 let user_name = prompt("What is your name?")
 alert(`Welcome, ${user_name}, to Haunted St. Matthew HS!`)
-let choice = +prompt("Do you want to go into the (1) library or (2) basement?")
+let choice = +prompt("Do you want to go into the (1) library, (2) basement, or (3) Gymnasium?")
 if (choice == 1) {
+    let image = document.getElementById("bg")
+ //   document.getElementById("bg").src="./image/library.jpg"
+    image.style.backgroundImage = "url('./image/library.jpg')";
+    image.style.backgroundRepeat = "no-repeat";
+    image.style.backgroundPosition = "center";
+
     library()
 }
 if (choice == 2) {
     basement()
 }
+
+if(choice == 3) {
+    gymnasium()
+}
+}
+
+function continue_game() {
+    let choice = +prompt("Do you want to go into the (1) library, (2) basement, or (3) Gymnasium?")
+    if (choice == 1) {
+        let image = document.getElementById("bg")
+     //   document.getElementById("bg").src="./image/library.jpg"
+        image.style.backgroundImage = "url('./image/library.jpg')";
+        image.style.backgroundRepeat = "no-repeat";
+        image.style.backgroundPosition = "center";
+    
+        library()
+    }
+    if (choice == 2) {
+        basement()
+    }
+    if(choice == 3) {
+        gymnasium()
+    }
 }
 
 function library() {
+    alert("Welcome To The Library!")
+    alert("Find a way to escape without being killed by a ghost!")
     let option = +prompt("You see a creepy old book on a dusty table. Do you (1) open it or (2) leave it alone?")
     
     if (option == 1){
-        alert("A ghost emerges from the book and scares you to death!")
-        alert("The End. Thanks for Playing!")
+        alert("You open the book and find a key inside.")
+        let run = +prompt("You walk around and find a exit, but you see a ghost nearby, do you (1) run to the exit or (2) hide till the ghost leaves?")
+        if (run == 1) {
+            alert("You run for the exit but the ghost spots you and you DIE.")
+            alert("You Failed To Escape!")
+            let cont = +prompt("Would you like to (1) continue or (2) leave?") 
+            if (cont == 1) {
+                continue_game()
+            }
+            if (cont == 2) {
+                alert("The end. Thanks for playing!")
+            }
+        }
+        if (run == 2) {
+            alert("The ghost leaves, you find your way to the exist and escape.")
+            alert("You Survived The Library!")
+            let cont = +prompt("Would you like to (1) continue or (2) leave?") 
+            if (cont == 1) {
+                continue_game()
+            }
+            if (cont == 2) {
+                alert("The end. Thanks for playing!")
+            }
+        }
+        /*  alert("A ghost emerges from the book and scares you to death!")
+        alert("The End. Thanks for Playing!") */
     }
 
     if (option == 2) {
-        alert("You quietly leave the library and find the exit. You survive!")
-        alert("The End. Thanks for Playing!")
+        alert("You ignore the book and walk around the library.")
+        alert("You find the exit but it's locked. You search for the key and see a ghost near the creepy book you saw before.")
+        let escape = +prompt("You realize the key is in the book, do you (1) wait for the ghost to leave and snatch the book or (2) sneak behind the ghost and snatch the book or (3) Ignore the ghost and look for another exit.")
+
+        if (escape == 1) {
+            alert("The ghost leaves. You grab the book and find the key inside.")
+            alert("You search for the exit, but you see a shiny mysterious bottle on the ground.")
+            let die = +prompt("Do you (1) ignore it or (2) pick it up?")
+            if (die == 1) {
+                alert("you find your way to the exit and you're able to escape.")
+                alert("You Survived The Library!")
+                let cont = +prompt("Would you like to (1) continue or (2) leave?") 
+                if (cont == 1) {
+                    continue_game()
+                }
+                if (cont == 2) {
+                    alert("The End. Thanks for playing!")
+                }
+            }
+            if (die == 2) {
+                alert("You pick the bottle and accidentally dropped it, alerting the ghost.")
+                alert("You run for the exit but the ghost spots you and you DIE.")
+                alert("You Failed To Escape!")
+                let cont = +prompt("Would you like to (1) continue or (2) leave?") 
+                if (cont == 1) {
+                    continue_game()
+                }
+                if (cont == 2) {
+                    alert("The end. Thanks for playing!")
+                }
+
+            }
+
+                
+            /*  alert("You quietly leave the library and find the exit. You survive!")
+            alert("The End. Thanks for Playing!") */
+
+        }
+
+        if (escape == 2) {
+            alert("The ghost sees you and rushes towards you and you DIE.")
+            alert("You Failed To Escape!")
+            let cont = +prompt("Would you like to (1) continue or (2) leave?") 
+            if (cont == 1) {
+                continue_game()
+            }
+            if (cont == 2) {
+                alert("The end. Thanks for playing!")
+            }
+        }
+
+        if (escape == 3) {
+            alert("You walk to the opposite end of library and find another exit.")
+            alert("In order to open it you must solve a riddle to unlock the door without alerting the ghost.")
+            alert("The riddle is: 'I don’t have eyes, but once I did see. Once I had thoughts, but now I’m white and empty. What am I?'")
+            let answer = +prompt("Is it (1) a skull or (2) a candle")
+            if (answer == 1) {
+                alert("You solve the riddle and the door unlocks. You escape without alerting the ghost and survive.")
+                let cont = +prompt("Would you like to (1) continue or (2) leave?") 
+                if (cont == 1) {
+                    continue_game()
+                }
+                if (cont == 2) {
+                    alert("The end. Thanks for playing!")
+                }
+
+            }
+            if (answer == 2) {
+                alert("You picked the wrong answer.")
+                alert("The ghost spots you and you DIE.")
+                alert("You Failed To Escape!")
+                let cont = +prompt("Would you like to (1) continue or (2) leave?") 
+                if (cont == 1) {
+                    continue_game()
+                }
+                if (cont == 2) {
+                    alert("The end. Thanks for playing!")
+                }
+
+            }
+
+        }
     }
+    
 
 }
 
